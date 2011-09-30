92c486fa9ee80e5706ff5610182badadef59b55f
CedarRapidsBoy@gmail.com <CedarRapidsBoy@gmail.com@34515e2d-a7f1-1bb0-4432-4592ae73068f>
2011-09-30 00:03:55 +0000

    cleaned up the electron calculation

--- trunk/lcarswallpaper/src/com/example/android/maxpapers/lcars/ElectronCalcThread.java
+++ trunk/lcarswallpaper/src/com/example/android/maxpapers/lcars/ElectronCalcThread.java
@@ -3,38 +3,31 @@ package com.example.android.maxpapers.lcars;
 import java.util.Date;
 
 public class ElectronCalcThread extends AbstractThread {
-	private float x1;
-	private float y1;
+	private double x1;
+	private double y1;
+	private long time;
 
-	private float xScale;
-	private float yScale;
 
-	public ElectronCalcThread(float xScale, float yScale, int poll) {
+	public ElectronCalcThread(int poll) {
 		super(poll);
-		this.xScale = xScale;
-		this.yScale = yScale;
 	}
 
 	@Override
 	protected void doStuff() {
-		long time = new Date().getTime() / 33;
-		float x1a;
-		float y1a;
-		x1a = new Double((xScale * (940)) + (xScale * (83))
-				* Math.cos((time % 360) * (Math.PI / 180))).floatValue();
-		y1a = new Double((yScale * (439)) + (yScale * (83))
-				* Math.sin((time % 360) * (Math.PI / 180))).floatValue();
-		x1 = x1a;
-		y1 = y1a;
+		synchronized (this) {
+			time = new Date().getTime() / 33;
+			x1 = 940 + (83 * Math.cos((time % 360.0) * (Math.PI / 180.0)));
+			y1 = 439 + (83 * Math.sin((time % 360.0) * (Math.PI / 180.0)));
+		}
 		pauseThread();
 	}
 
 	public float getY1() {
-		return y1;
+		return (float) y1;
 	}
 
 	public float getX1() {
-		return x1;
+		return (float) x1;
 	}
 
 }
